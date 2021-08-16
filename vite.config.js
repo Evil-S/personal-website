0882fa57c438562725850a805bcc3e1b13bef467
minjie.shao <minjie.shao@geely.com>
2021-08-16 16:55:07 +0800

    feat: 开发使用vite

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import {defineConfig} from 'vite'
+import {createVuePlugin} from 'vite-plugin-vue2'
+import {resolve} from 'path'
+
+function pathResolve(dir) {
+  return resolve(process.cwd(), '.', dir)
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+  },
+  plugins: [
+    createVuePlugin({
+      vueTemplateOptions: {}
+    }),
+  ],
+  resolve: {
+    extensions: ['.vue', '.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+    alias: [
+      {
+        find: /\/@\//,
+        replacement: pathResolve('src') + '/',
+      }
+    ]
+  },
+})
